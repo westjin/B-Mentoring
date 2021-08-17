dfee8859fd1cbc73d0b4eb070364742b9e9c2d27
vmware_win10 <soft1003@gmail.com>
2021-08-17 22:13:48 +0900

    Revert "주석 처리"
    This reverts commit ccf0db7424856f35b7cb0ef1edce6e5c3d5ad3ee.

--- src/main/java/net/itdaa/cau/study/entity/RoadAddrIntg.java
+++ src/main/java/net/itdaa/cau/study/entity/RoadAddrIntg.java
@@ -7,9 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- * JPA 에서 Entity 객체는 사용하는 DB 의 Table 과 구조가 같아야 합니다.
- */
 @Entity
 @Data
 @Table(name="ROAD_ADDR_INTG")
@@ -17,29 +14,29 @@ public class RoadAddrIntg {
 
     @Id
     @Column(name = "MGR_NO")
-    private String mgrNO;                 // 관리번호 (PK)
+    private String mgrNO;
 
     @Column(name = "SIDO_NAME")
-    private String sidoName;              // 시도명 
+    private String sidoName;
 
     @Column(name = "SIGUNGU_NAME")
-    private String sigunguName;           // 시군구명
+    private String sigunguName;
 
     @Column(name = "ROAD_NAME")
-    private String roadName;              // 도로명
+    private String roadName;
 
     @Column(name = "BLDG_MAIN_NO")
-    private Integer bldgMainNo;           // 빌딩본번
+    private Integer bldgMainNo;
 
     @Column(name = "BLDG_SUB_NO")
-    private Integer bldgSubNo;            // 빌딩부번
+    private Integer bldgSubNo;
 
     @Column(name = "CONST_BLDG_NAME")
-    private String constBldgName;         // 건축물대장의 빌딩명
+    private String constBldgName;
 
-    @Column(name = "POST_CODE") 
-    private String postCode;              // 우편번호
+    @Column(name = "POST_CODE")
+    private String postCode;
 
     @Column(name = "FULL_ROAD_ADDR")
-    private String fullRoadAddr;          // 전체 도로명 주소
+    private String fullRoadAddr;
 }
